189b1e6b209b61a77f6899661da256b2663c89b5
Jinghao Yu <markyudan5@gmail.com>
2020-03-15 00:05:03 +1100

    test file

--- src/Components/PackingSplips/PackingSlips.jsx
+++ src/Components/PackingSplips/PackingSlips.jsx
@@ -6,5 +6,14 @@ import NavButton from '../UI/Navbar';
 
 // import { Redirect } from 'react-router-dom'
 export class PackingSpips extends React.Component{
+    constructor(props) {
+        super(props);
 
+    }
+    render() {
+        return (
+            <h1>Test</h1>;
+
+        );
+    }
 }
