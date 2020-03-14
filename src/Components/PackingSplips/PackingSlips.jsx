6f9908d7a06bbe5b6f550bb1c2363ab65bbab3dd
jinghaoyu <jinghaoyu@jinghaoyudeiMac.local>
2020-03-14 19:06:07 +1100

    file creation

---
+++ src/Components/PackingSplips/PackingSlips.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//import components
+// import FakeAuth from '../FakeAuth';
+import NavButton from '../UI/Navbar';
+
+// import { Redirect } from 'react-router-dom'
+export class PackingSpips extends React.Component{
+
+}
