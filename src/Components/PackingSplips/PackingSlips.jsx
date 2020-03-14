88d75b656d7d4c711fa320b7c1c11a5eff972702
Jinghao Yu <markyudan5@gmail.com>
2020-03-15 00:37:55 +1100

    simple template for demo

--- src/Components/PackingSplips/PackingSlips.jsx
+++ src/Components/PackingSplips/PackingSlips.jsx
@@ -12,7 +12,67 @@ export class PackingSpips extends React.Component{
     }
     render() {
         return (
-            <h1>Test</h1>;
+            <div>
+                <h1>Packing Slips</h1>
+            </div>
+            <div>
+                <h2>Filter Orders By:</h2>
+                <table>
+                    <tr>
+                        <th>Delivery Date:</th>
+                        <td>
+                            <select id="" name="">
+                                <option value="none">2/2/2020</option>
+                            </select>
+                        </td>
+                        <th>Customer Name:</th>
+                        <td>
+                            <select id="" name="">
+                                <option value="none">Bob</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Customer ID:</th>
+                        <td>
+                            <select id="" name="">
+                                <option value="none">123456</option>
+                            </select>
+                        </td>
+                        <th>Invoice Number:</th>
+                        <td>
+                            <select id="" name="">
+                                <option value="none">123456</option>
+                            </select>
+                        </td>
+                    </tr>
+
+                </table>        
+                
+            </div>
+            <div>
+                <h2>Results</h2>
+                <table>
+                    <tr>
+                        <th>Customer ID</th>
+                        <th>Customer Name</th>
+                        <th>Inovice Number</th>
+                        <th>Delivery Date</th>
+                    </tr>
+                    <tr>
+                        <td>123456</td>
+                        <td>Bob</td>
+                        <td>123456</td>
+                        <td>2/2/2020</td>
+                    </tr>
+                    <tr>
+                        <td>223456</td>
+                        <td>Alex</td>
+                        <td>111111</td>
+                        <td>2/3/2020</td>
+                    </tr>
+                </table>
+            </div>
 
         );
     }
