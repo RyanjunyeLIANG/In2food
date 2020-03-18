9db75d4128dbbf0bcc2e104e11486e7ac5788e07
Ryan.LIANG <32515067+RyanjunyeLIANG@users.noreply.github.com>
2020-03-18 13:02:37 +1100

    Add comments on dashboardrouter

--- src/Components/Routers/DashboardRouter.jsx
+++ src/Components/Routers/DashboardRouter.jsx
@@ -3,14 +3,19 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 //import components
 import PackingSlips from '../Dashboard/PackingSlips';
+// import Inventory from '../Dashboard/Inventory';
+
 
 export default function DashboardRouter() {
   return (
   <Switch>
+    {/* Default page of dashboard */}
     <Route exact path="/dashboard">
       <Redirect to="/dashboard/packing-slip" /> 
     </Route>
-    <Route path="/dashboard/inventory" />
+
+    {/* Side pages */}
+    {/* <Route path="/dashboard/inventory" component={ Inventory }/> */}
     <Route path="/dashboard/packing-slip" component={ PackingSlips } />
   </Switch>
   );
