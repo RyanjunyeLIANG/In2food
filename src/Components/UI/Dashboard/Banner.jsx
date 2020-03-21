3db52e567cc6ce394f72b1b8e7cacca176c4b229
jinghaoyu <jinghaoyu@jinghaoyudeiMac.local>
2020-03-21 19:14:55 +1100

    delete to fix "login.jsx"

--- src/Components/UI/Dashboard/Banner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Banner(props) {
-  return (
-  <div name="banner" className="margin-fix padding-fix">
-    <h1 className="text-center"> {props.name} </h1>
-  </div>
-  );
-}
